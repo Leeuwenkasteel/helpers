1a494a675546d8f4303166eee3d6db7dd1ab0b7d
Leeuwenkasteel <sanne@leeuwenkasteel.nl>
2022-07-01 12:15:22 +0200

    first input

---
+++ src/HelpersServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Leeuwenkasteel\Helpers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+
+class HelpersServiceProvider extends ServiceProvider
+{
+    public function boot(){
+        
+		
+    }
+
+    public function register(){
+		 
+    }
+}
